a526800d1e8181f5d4f8ebff2a9a82a5ba27de2a
Cimbir <stepdachis@gmail.com>
2024-06-13 19:55:09 +0400

    Added description and state columns to quizzes table

--- jQuiz/database_structure/quizzes.sql
+++ jQuiz/database_structure/quizzes.sql
@@ -10,5 +10,10 @@ CREATE TABLE quizzes
     created_at DATETIME,
     time INT,
     thumbnail VARCHAR(100),
+    shouldMixUp BIT,
+    showAll BIT,
+    autoCorrect BIT,
+    allowPractice BIT,
+    description TEXT,
     FOREIGN KEY (author_id) REFERENCES users(id)
 );
