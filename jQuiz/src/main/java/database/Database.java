21adc20136cc4c85bc0758f50bf7074915686f7b
Cimbir <stepdachis@gmail.com>
2024-06-13 11:23:37 +0400

    database interface to abstract class

--- jQuiz/src/main/java/database/Database.java
+++ jQuiz/src/main/java/database/Database.java
@@ -1,10 +1,16 @@
 package database;
 
+
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
 import java.util.List;
 
-public interface Database<T> {
-    List<T> getALL();
-    T getById(int id);
-    boolean setById(int id, T toChange);
-    boolean removeById(int id);
+public abstract class Database<T> {
+    private BasicDataSource dataSource;
+
+    public abstract List<T> getAll();
+    public abstract T getById(int id);
+    public abstract boolean removeById(int id);
+    public abstract boolean setById(int id, T toChange);
 }
