d7c8692be6c533bcf5f79b7e0015bc512838e697
Cimbir <stepdachis@gmail.com>
2024-06-14 14:16:00 +0400

    Database abstract class implementation

--- jQuiz/database_structure/users.sql
+++ jQuiz/database_structure/users.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE users
 (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50),
     is_admin BOOL,
     created_at DATETIME,
@@ -12,3 +12,11 @@ CREATE TABLE users
     pass VARCHAR(50),
     image VARCHAR(100)
 );
+
+INSERT INTO users (username, is_admin, created_at, email, pass, image) VALUES ('luka', true, sysdate(), 'lt', '123', 'tmpimg');
+INSERT INTO users (username, is_admin, created_at, email, pass, image) VALUES ('dachi', true, sysdate(), 'ds', '113', 'tmpimg1');
+INSERT INTO users (username, is_admin, created_at, email, pass, image) VALUES ('gio', true, sysdate(), 'ge', '1234', 'tmp1img');
+
+SELECT * FROM users where id = 1;
+
+UPDATE users SET username = 'Tura' Where id = 1
--- jQuiz/src/main/java/database/Database.java
+++ jQuiz/src/main/java/database/Database.java
@@ -4,13 +4,48 @@ package database;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public abstract class Database<T> {
     private BasicDataSource dataSource;
+    private String databaseName;
 
-    public abstract List<T> getAll();
-    public abstract T getById(int id);
-    public abstract boolean removeById(int id);
-    public abstract boolean setById(int id, T toChange);
+    public List<T> getAll() throws ClassNotFoundException, SQLException {
+        ResultSet rs = getResultSet("SELECT * FROM " + databaseName);
+        List<T> res = new ArrayList<T>();
+        while (rs.next()){
+            res.add(getItemFromResultSet(rs));
+        }
+        return res;
+    }
+    public T getById(int id) throws SQLException, ClassNotFoundException {
+        ResultSet rs = getResultSet("SELECT * FROM " + databaseName + " WHERE id = " + id);
+        return getItemFromResultSet(rs);
+    }
+    public boolean removeById(int id) throws SQLException, ClassNotFoundException {
+        PreparedStatement statement = getStatement("DELETE FROM " + databaseName + " WHERE id = " + id);
+        return statement.executeUpdate() > 0;
+    }
+    public boolean setFieldById(int id, String columnName, String value) throws SQLException, ClassNotFoundException {
+        PreparedStatement statement = getStatement("UPDATE " + databaseName + " SET " + columnName + " = " + value + " WHERE id = " + id);
+        return statement.executeUpdate() > 0;
+    }
+    public abstract boolean add(T toAdd);
+    protected abstract T getItemFromResultSet(ResultSet rs);
+    private ResultSet getResultSet(String sqlQuery) throws ClassNotFoundException, SQLException {
+        PreparedStatement statement = getStatement(sqlQuery);
+        statement.executeQuery();
+        return statement.getResultSet();
+    }
+    private PreparedStatement getStatement(String sqlQuery) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = dataSource.getConnection();
+        return con.prepareStatement(sqlQuery);
+    }
 }
