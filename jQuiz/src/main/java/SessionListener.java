734ce8f3056beb80eb19b467995d090353b33162
Cimbir <stepdachis@gmail.com>
2024-06-17 18:10:16 +0400

    Added session listener

--- jQuiz/src/main/java/SessionListener.java
+++ jQuiz/src/main/java/SessionListener.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class SessionListener {
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+public class SessionListener implements HttpSessionListener {
+    @Override
+    public void sessionCreated(HttpSessionEvent httpSessionEvent) {
+
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
+
+    }
 }
