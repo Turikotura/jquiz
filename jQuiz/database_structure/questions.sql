cd481ed6b368a66759e3413a5741954f56731f5d
turikotura <turikotura@outlook.com>
2024-06-19 17:32:26 +0400

    Small fxi

--- jQuiz/database_structure/questions.sql
+++ jQuiz/database_structure/questions.sql
@@ -3,7 +3,7 @@ USE quizDB;
 CREATE TABLE questions
 (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    question_type_id INT,
+    question_type INT,
     text TEXT,
     quiz_id INT,
     image_url VARCHAR(100),
@@ -11,17 +11,17 @@ CREATE TABLE questions
     FOREIGN KEY (quiz_id) REFERENCES quizzes(id)
 );
 
-INSERT INTO questions (question_type_id, text, quiz_id, image_url, score)
+INSERT INTO questions (question_type, text, quiz_id, image_url, score)
     VALUES (0,'Name the first US president',1,NULL,1);
-INSERT INTO questions (question_type_id, text, quiz_id, image_url, score)
+INSERT INTO questions (question_type, text, quiz_id, image_url, score)
 VALUES (0,'Name the president who wrote the declaration of independence',1,NULL,1);
-INSERT INTO questions (question_type_id, text, quiz_id, image_url, score)
+INSERT INTO questions (question_type, text, quiz_id, image_url, score)
 VALUES (4,'Who was the president during the US civil war?',1,NULL,1);
 
-INSERT INTO questions (question_type_id, text, quiz_id, image_url, score)
+INSERT INTO questions (question_type, text, quiz_id, image_url, score)
 VALUES (5,'Select all the presidents who were elected in the 21st century.',1,NULL,1);
 
-# INSERT INTO questions (question_type_id, text, quiz_id, image_url, score)
+# INSERT INTO questions (question_type, text, quiz_id, image_url, score)
 #     VALUES (1, 'Name US presidents', 2, NULL, 1);
-# INSERT INTO questions (question_type_id, text, quiz_id, image_url, score)
+# INSERT INTO questions (question_type, text, quiz_id, image_url, score)
 #     VALUES (2, 'Name world continents', 2, NULL, 1);
