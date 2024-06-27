6462ad13f0278190afe28a621a8e38e54721a65b
Cimbir <stepdachis@gmail.com>
2024-06-27 14:35:17 +0400

    Added so that you can't grade again after grading it once. Fixed the structure of the tests

--- jQuiz/src/main/java/attempts/QuestionAttempt.java
+++ jQuiz/src/main/java/attempts/QuestionAttempt.java
@@ -13,6 +13,10 @@ public class QuestionAttempt {
     private int correctAnswersAmount;
     private int maxScore;
 
+    // Used to not grade a question more than once
+    private int gottenGrade;
+    private boolean wasGraded;
+
     /**
      * Constructor for QuestionAttempt
      * @param question - question object
@@ -21,7 +25,7 @@ public class QuestionAttempt {
     public QuestionAttempt(Question question, List<Answer> answers){
         this.question = question;
         this.writtenAnswers = new ArrayList<>();
-        this.answers = answers;
+        this.answers = new ArrayList<>(answers);
         Collections.shuffle(this.answers);
         this.maxScore = question.getScore();
 
@@ -32,6 +36,9 @@ public class QuestionAttempt {
             }
         }
         correctAnswersAmount = corAns.size();
+
+        this.wasGraded = false;
+        this.gottenGrade = 0;
     }
 
     public Question getQuestion() {return question;}
@@ -44,13 +51,21 @@ public class QuestionAttempt {
      * Sets the answers written by user
      * @param answers - answers written by user
      */
-    public void setWrittenAnswers(List<String> answers) {writtenAnswers = answers;}
+    public void setWrittenAnswers(List<String> answers) {
+        if(wasGraded) {
+            return;
+        }
+        writtenAnswers = new ArrayList<>(answers);
+    }
 
     /**
      * Get score of question given the answers
      * @return the score of the question
      */
     public int evaluateAnswers(){
+        if(wasGraded){
+            return gottenGrade;
+        }
         Set<Answer> corAns = new HashSet<>();
         int wrongAnswers = 0;
         for(String wrAns : writtenAnswers){
@@ -69,6 +84,8 @@ public class QuestionAttempt {
         if(question.getQuestionType() == QuestionTypes.MULTI_ANS_MULTI_CHOICE){
             correctAnswers = Math.max(0, correctAnswers-wrongAnswers);
         }
-        return maxScore * correctAnswers / correctAnswersAmount;
+        wasGraded = true;
+        gottenGrade = maxScore * correctAnswers / correctAnswersAmount;
+        return gottenGrade;
     }
 }
