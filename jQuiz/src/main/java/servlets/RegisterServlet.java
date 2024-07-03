3f4f49fca535c90342baddbc7c135d33b5273abb
turikotura <turikotura@outlook.com>
2024-07-03 21:09:14 +0400

    Added user profile image upload during registration

--- jQuiz/src/main/java/servlets/RegisterServlet.java
+++ jQuiz/src/main/java/servlets/RegisterServlet.java
@@ -5,25 +5,44 @@ import accounts.Security;
 import database.*;
 import models.User;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.SQLException;
 import java.util.Date;
 
+@MultipartConfig(maxFileSize = 16177215)
 public class RegisterServlet extends HttpServlet {
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         response.setContentType("text/html");
         String userName = request.getParameter("user-name"),
                 email = request.getParameter("email"),
                 password1 = request.getParameter("password1"),
-                password2 = request.getParameter("password2"),
-                imageLink = request.getParameter("profile-pic");
+                password2 = request.getParameter("password2");
+
+        byte[] image = null;
+        Part filePart = request.getPart("photo");
+        if (filePart != null) {
+            try (InputStream inputStream = filePart.getInputStream();
+                 ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, bytesRead);
+                }
+                image = outputStream.toByteArray();
+            }
+        }
 
         try {
-            int result = Register.createNew(userName,email,password1,password2,imageLink,(UserDatabase)request.getServletContext().getAttribute(Database.USER_DB));
+            int result = Register.createNew(userName,email,password1,password2,image,(UserDatabase)request.getServletContext().getAttribute(Database.USER_DB));
             if(result == Register.USERNAME_EXISTS) {
                 request.getServletContext().setAttribute("reg-message","User named " + userName + " already exists.");
                 response.sendRedirect("register.jsp");
@@ -39,7 +58,7 @@ public class RegisterServlet extends HttpServlet {
                         "Password must contain at least one uppercase letter, one lowercase letter and one digit.");
                 response.sendRedirect("register.jsp");
             } else {
-                User newUser = new User(User.NO_ID,userName,new Date(),email, Security.getHash(password1),null,imageLink);
+                User newUser = new User(User.NO_ID,userName,new Date(),email, Security.getHash(password1),image,null);
                 ((UserDatabase)request.getServletContext().getAttribute(Database.USER_DB)).add(newUser);
                 newUser = ((UserDatabase)request.getServletContext().getAttribute(Database.USER_DB)).getByUsername(newUser.getUsername());
                 request.getSession().setAttribute("curUser",newUser);
